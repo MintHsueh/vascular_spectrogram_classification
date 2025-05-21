277f2e9cd1f27a2d550ada9de61e1c214f67f918
Mint <minzhi0924@gmail.com>
2025-05-22 04:38:52 +0800

    update pyaudio_training_resnet50.py and README.md

---
+++ spectrogram_classification/README.md
@@ -0,0 +1,170 @@
+# 模型訓練原理原理
+
+本專案模型訓練流程：
+
+* step 0. 將每段血管聲音週期轉成 spectrogram，組成 batch 丟進模型
+
+* step 1. 將 每一張 spectrogram 的 label 進行 one-hot 編碼
+
+* step 2. 每一張 spectrogram 會經過 ResNet 得到 logits，代表模型對於分類血管正常與狹窄的信心分數
+
+* step 3. 用 softmax 將 logits 轉成機率
+
+* step 4. 計算 Binary Cross Entropy Loss
+
+* step 5. 反向傳播
+
+以下將用簡單例子說明 step1~4 的運作機制
+
+---
+### step 1. Label One-Hot 編碼
+假如 labels 存放三筆資料對應的 label，0 為血管正常，1 為血管狹窄
+```
+labels = [0, 1, 1]
+```
+對三個 label 進行 one hot 編碼後，存放於 onehot_labels
+```
+onehot_labels = [[1, 0],
+                 [0, 1],
+                 [0, 1]]
+```
+---
+### step 2. 模型輸出 logits
+Logits 是模型最後一層的輸出，代表每個類別的原始信心分數。
+```
+logits = [[2.0, 0.5],   # 第一筆模型覺得第 0 類比較有信心
+          [0.1, 3.0],   # 第二筆模型覺得第 1 類比較有信心
+          [1.5, 1.5]]   # 第三筆模型兩類信心一樣
+```
+---
+### step 3. logits 經過 softmax 函數轉換
+需要利用 softmax 將 Logits 轉成機率，後續才能配合 one-hot label，計算 Binary Cross Entropy Loss。
+```
+probabilities = [[0.8176, 0.1824],  # 第一筆預測為「正常」的機率是 81.8%，
+                 [0.0522, 0.9478],  # 第二筆預測為「狹窄」機率是 94.8%，
+                 [0.5000, 0.5000]]  # 第三筆預測「正常」與「狹窄」的機率各為 50%。
+```
+---
+### step 4. 計算 Loss
+根據模型預測的機率，和真實標籤的差距，計算出一個 loss 數值，loss 值越小越準確。
+Binary Cross Entropy Loss 公式如下：
+```
+Loss = - [y * log(p) + (1 - y) * log(1 - p)]
+```
+
+計算三筆資料的 Binary Cross Entropy Loss：
+```
+# 第一筆資料的 loss
+Loss1 = - (1 * log(0.8176) + 0 * log(1 - 0.1824)) ≈ -log(0.8176) ≈ 0.201
+
+# 第二筆資料的 loss
+Loss2 = - (0 * log(0.0522) + 1 * log(0.9478)) ≈ -log(0.9478) ≈ 0.053
+
+# 第三筆資料的 loss
+Loss3 = - (0 * log(0.5) + 1 * log(0.5)) = -log(0.5) ≈ 0.693
+
+# 三筆 loss 平均
+Loss = (Loss_1 + Loss_2 + Loss_3) / 3 ≈ 0.316
+```
+---
+### Notes
+如果用 nn.CrossEntropyLoss() 的話，就不需要自己做 softmax，因為它會內建處理 logit → softmax → log loss。但因為此專案使用 BCELoss() 搭配 one-hot label，所以才要加上 softmax 以對應 label 格式。
+
+<!-- ---
+假設你有以下設定：
+假設 batch_size = 3: 一個 batch 拿 3 筆 spectrogram 資料
+
+以下以一個batch的流程做說明:
+
+對應的標籤是：[0, 1, 1]
+
+每筆 spectrogram 是 1×40×100 的大小（1通道、40個頻率、100個時間點）
+
+batch_size = 3
+
+---
+### 1. Label One-Hot 編碼範例
+
+使用 `torch.nn.functional.one_hot` 將數值標籤轉換為 one-hot 向量格式，配合 BCELoss 使用。
+
+```python
+import torch
+import torch.nn.functional as F
+
+# 假設 labels_tmp 存放三筆資料對應的 label 
+images.shape = (3, 1, 40, 100)   # 3張圖片、1通道、40個頻率、100個時間點
+labels_tmp = torch.tensor([0, 1, 1])
+
+# 將 labels_tmp 中每一個 label 做 one-hot 編碼
+labels = F.one_hot(labels_tmp, num_classes=2).float().to(device)
+
+# 輸出結果
+# labels = tensor([[1., 0.],
+#                  [0., 1.],
+#                  [0., 1.]])
+```
+## 2. 模型前向傳播
+把 spectrogram 圖片（images）丟進模型 model，模型會跑一輪 forward，輸出每筆資料的 logits
+* Logits 是模型最後一層的輸出，它們本身不是機率，只是代表每個類別的原始信心分數，我們會再用 softmax 把它轉成機率，才能配合 one-hot label 計算 loss。
+```python
+model = resnet50(weights=None)
+outputs = model(images)
+
+# 輸出結果
+# outputs = [[2.1, 0.3], 
+#            [0.5, 1.7], 
+#            [1.2, 1.2]]   # shape = (batch_size, 2)
+
+softmax = nn.Softmax(dim=1).to(device)
+outputs = softmax(outputs)
+
+# 輸出結果
+# outputs = [[0.8176, 0.1824],
+#            [0.0522, 0.9478],
+#            [0.5000, 0.5000]]
+
+```
+
+代表第一筆預測「0 類」的機率是 81.8%，第二筆預測 1 類機率是 94.8%，第三筆不確定，兩邊各 50%。
+
+## 3. 計算 loss
+➡️「根據模型預測的機率，和真實標籤的差距，計算出一個 loss 數值」
+➡️ 這個 loss 表示模型在這個 batch 預測得好不好，值越小越準確。
+```python
+criterion = nn.BCELoss().to(device)
+loss_list = []
+
+loss = criterion(outputs,labels)
+loss_list.append(loss.item())   # .item() 是 PyTorch 的寫法，意思是把 loss 這個 Tensor 轉成純數字（float）
+
+# loss ≈ 0.316
+```
+### Binary Cross Entropy Loss 計算：
+```
+Binary Cross Entropy Loss 公式
+Loss = - [y * log(p) + (1 - y) * log(1 - p)]
+
+第一筆資料的 loss
+Loss1 = - (1 * log(0.8176) + 0 * log(1 - 0.1824)) ≈ -log(0.8176) ≈ 0.201
+
+第二筆資料的 loss
+Loss2 = - (0 * log(0.0522) + 1 * log(0.9478)) ≈ -log(0.9478) ≈ 0.053
+
+第三筆資料的 loss
+Loss3 = - (0 * log(0.5) + 1 * log(0.5)) = -log(0.5) ≈ 0.693
+
+此範例 loss
+Loss = (Loss_1 + Loss_2 + Loss_3) / 3 ≈ 0.316
+``` -->
+
+
+
+
+
+
+
+
+
+
+
+
--- spectrogram_classification/pyaudio_training_resnet50.py
+++ spectrogram_classification/pyaudio_training_resnet50.py
@@ -124,7 +124,7 @@ class trainset(Dataset):
             img = img.cpu()
             img = F.interpolate(img, size=100)  # 圖片統一大小 (時間軸都補成100)
             imgs.append(img)
-        return copy.deepcopy(imgs)
+        return copy.deepcopy(imgs)  # img.shape = (1, 40, 100)
     
         # Notes:
         # sample rate = 22050 → 一秒採集 22050 個點，決定「最多能聽到多高頻」
@@ -139,7 +139,7 @@ def main():
     patient_list_intersection = list(patient_list_intersection) # 同時有 normal 與 abnormal 資料的病人編號
     patient_list_total        = list(patient_list_total)    # 所有病人編號
 
-    softmax = nn.Softmax(dim=1).to(device)  # softmax 函數，為了轉換 nn model 的 output 結果為機率值
+    softmax = nn.Softmax(dim=1).to(device)  # softmax 函數，dim = 1 為在每一筆資料的類別維度上做 softmax (outputs 的 shape = (3, 2) → 表示有 3 筆資料，每筆資料有 2 類別的分數)
 
     num_repeats = 2 # for test
     accuracy_list = []
@@ -162,8 +162,6 @@ def main():
         train_abnormal_wavs , train_abnormal_wavs_paths = load_wavs(abnormal_file_path,patient_train_list)   # train set 病人的所有 abnormal 音檔的 waveform 及路徑
 
         print('\ntrain set patient:', patient_train_list, ', test set patient:', patient_test_list)
-        # print('test set normal sample:', len(test_normal_wavs), ', test set abnormal sample:', len(test_abnormal_wavs))
-        # print('train set normal sample:',len(train_normal_wavs), ', train set abnormal sample:', len(train_abnormal_wavs))
 
         train_dataset = trainset(train_normal_wavs[:len(train_abnormal_wavs)],train_abnormal_wavs)  # 建立 train dataset (時頻譜img + label 的清單)
         test_dataset  = trainset(test_normal_wavs[:len(test_abnormal_wavs)],test_abnormal_wavs)     # 建立 test dataset (時頻譜img + label 的清單)
@@ -177,21 +175,22 @@ def main():
 
         # Part 2. 創建 ResNet50 模型
         model = resnet50(weights=None)  # 載入 torchvision 內建的 ResNet50 模型架構 (weights=None 表示不使用 ImageNet 預訓練權重，從頭訓練)
-        model.fc = nn.Linear(2048,2)    # 2 為輸出層為二分類 (normal vs. abnromal)
-        model.conv1 = nn.Conv2d(1, 64, kernel_size=(5, 5), stride=(2, 2), padding=(3, 3), bias=False)   # 修改第一層卷積層: 輸入通道數 = 1 (spectrogram 是灰階)、64 個 kernel map 數量
+        model.fc = nn.Linear(2048,2)    # 原本 ResNet50 預設是 1000 類 → 改為輸出 2 類（二分類）
+        model.conv1 = nn.Conv2d(1, 64, kernel_size=(5, 5), stride=(2, 2), padding=(3, 3), bias=False)   # conv1 的 in_channels=3 改為 1，因為 spectrogram 為灰階 (原本是RGB 彩圖)
         model = model.to(device)
 
-        num_epochs = 1  # 1用來測試, 原設50  # 每輪 epoch 會跑 (訓練集資料總數 / batch_size) step，每個 step 只拿 batch_size 筆資料訓練，共訓練 num_epochs 輪
-        batch_size = 8  # 8用來測試, 原設80  # 一次丟給模型的小批量資料 (一個 batch 完成一次 forward + backward 更新，就是完成 1 step)
+        num_epochs = 1  # 原設 50  # 設定訓練幾 epoch
+        batch_size = 8  # 原設 80  # 一次丟給模型的小批量資料
         learning_rate = 0.001   # 控制模型參數更新的幅度
         print_interval = 20     # 每 20 step 顯示一次訓練結果（loss, acc, 預估剩餘時間）
         # save_folder = 'resnet50/'
 
-        criterion = nn.BCELoss().to(device) # Binary Cross Entropy Loss: 計算模型輸出的預測機率跟實際標籤差距
-        optimizer = optim.Adam(model.parameters(),lr = learning_rate)   # 用 Adam 優化器來更新模型權重
+        criterion = nn.BCELoss().to(device) # Binary Cross Entropy Loss
+        optimizer = optim.Adam(model.parameters(),lr = learning_rate)   # 使用 Adam 優化器來根據 loss 更新模型參數
 
-        train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)   # 建立訓練資料的 DataLoader 以批次訓練模型 (shuffle=True: 每個 epoch 前都會打亂資料順序，避免模型只記憶順序特徵)
-        test_loader  = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)   # 建立測試資料的 DataLoader
+        # DataLoader: 把資料集切成多個 batch
+        train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)   # shuffle=True: 每個 epoch 前都會打亂訓練資料順序，避免模型只記憶順序特徵)
+        test_loader  = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)   # shuffle=False: 測試資料不打亂
 
         total_step = len(train_loader)  # 計算每個 epoch 要訓練幾 step
         loss_list = []  # 記錄每一筆 batch 的 loss
@@ -200,40 +199,31 @@ def main():
 
         # Part 3. 模型訓練
         for epoch in range(num_epochs):
-            for i,(images,labels_tmp) in enumerate(train_loader):
-                # if(images.size(0) != batch_size):
-                #     break
-
+            for i,(images,labels_tmp) in enumerate(train_loader):   # 每次從 train_loader 拿一個 batch 進行訓練 (batch_size 張 spectrogram 與對應的 label)
                 images = images.to(device)
                 
-                # 把標籤轉成 one-hot 
-                # e.g. 原本labels_tmp = [0, 1, 0, 1], one-hot 編碼後 labels = [[1, 0], [0, 1], [1, 0], [0, 1]]
-                # 因為outputs = [[0.98, 0.02], [0.10, 0.90]...] # 預測是類別0的機率是98%, 預測是類別1的機率是90% ...]
-                # 需要把標籤也變成相同的格式（[1, 0] 或 [0, 1]），這樣用 nn.BCELoss()一一對應算出 loss
-                labels = np.zeros((len(labels_tmp), 2))
-                for label_idx in range(len(labels_tmp)):
-                    labels[label_idx][labels_tmp[label_idx]] = 1
-                labels = torch.Tensor(labels).to(device)
+                # 把標籤轉成 one-hot 編碼
+                labels = F.one_hot(labels_tmp, num_classes=2).float().to(device)
 
                 # 模型前向傳播，得到模型輸出的機率值（兩類的機率）
-                outputs = model(images)
-                outputs = softmax(outputs)
+                outputs = model(images)     # 模型輸出為 logits
+                outputs = softmax(outputs)  # 將 logits 利用 softmax 轉為機率
 
-                #　計算 Loss
+                # 計算 Loss
                 loss = criterion(outputs,labels)
-                loss_list.append(loss.item())
+                loss_list.append(loss.item())   # .item() 為 PyTorch 寫法，把 loss 這個 Tensor 轉成純數字（float）
 
-                # 反向傳播（更新模型參數）
-                optimizer.zero_grad()   # 清除舊的梯度
-                loss.backward()         # 反向傳播
-                optimizer.step()        # 更新權重
+                # 反向傳播
+                optimizer.zero_grad()   # 每次訓練前，先把上一輪計算的梯度清空 (PyTorch 預設會累加梯度 gradient accumulation)
+                loss.backward()         # 反向傳播，算出所有參數的梯度
+                optimizer.step()        # 根據算出的梯度，更新模型的權重參數
 
                 # 計算準確率
-                total = labels.size(0)
-                _,predicted = torch.max(outputs.data,1)
+                total = labels.size(0)  # 取得這個 batch 中的樣本數 (= batch_size)
+                _,predicted = torch.max(outputs.data,1) # predicted 為模型預測的類別（e.g. [0, 1, 0, 0, 1]）
                 correct = 0
                 for label_idx in range(len(predicted)):
-                    if(labels[label_idx][predicted[label_idx]] == 1):
+                    if(labels[label_idx][predicted[label_idx]] == 1):   # labels 是 one-hot 標籤
                         correct += 1
                 acc_list.append(correct/total)
 
@@ -253,8 +243,8 @@ def main():
         # Part 4. 模型測試
         model.eval()    #  PyTorch 模型的評估模式
 
-        with torch.no_grad(): # 不用算梯度、不用反向傳播，只做前向推理（inference），節省記憶體
-            # 設定正確率參數(correct / total)、混淆矩陣參數 
+        with torch.no_grad(): # 做 inference，不要記錄梯度
+            # 設定準確度參數(correct / total)、混淆矩陣參數 
             correct = 0
             total = 0
             normal_to_abnormal = 0
@@ -268,30 +258,27 @@ def main():
                 labels = labels.to(device)
                 
                 # 進行預測
-                outputs = model(images) # outputs 是模型輸出的兩類機率
+                outputs = model(images) # outputs 是模型輸出的 logits
                 _,predicted = torch.max(outputs.data,1) # torch.max(..., 1) 會取出最大值的 index，即預測的類別（0 或 1）
                 
-                # 計算總數
-                total += labels.size(0)
-
-                normals  = labels[labels == 0]
-                abnomals = labels[labels == 1]
+                # 計算樣本總數
+                total += labels.size(0) 
 
                 # 建立混淆矩陣
                 for i in range(len(labels)):
                     if(predicted[i] == labels[i] and labels[i] == 0):
-                        normal_to_normal    += 1
+                        normal_to_normal += 1
                     elif(predicted[i] != labels[i] and labels[i] == 0):
-                        normal_to_abnormal  += 1
+                        normal_to_abnormal += 1
                     elif(predicted[i] == labels[i] and labels[i] == 1):
-                        abnormal_to_abnormal  += 1
+                        abnormal_to_abnormal += 1
                     elif(predicted[i] != labels[i] and labels[i] == 1):
-                        abnormal_to_normal  += 1
+                        abnormal_to_normal += 1
                 
                 # 累加正確數量
                 correct += (predicted == labels).sum().item()
 
-        # 計算結果
+        # 計算此 batch 結果
         accuracy = (normal_to_normal + abnormal_to_abnormal) / total * 100
         sensitivity = abnormal_to_abnormal / (abnormal_to_abnormal + abnormal_to_normal) * 100  # = Recall = 異常有多少被正確分類（True Positive Rate）
         specificity = normal_to_normal / (normal_to_normal + normal_to_abnormal) * 100  # 正常有多少被正確分類（True Negative Rate）
